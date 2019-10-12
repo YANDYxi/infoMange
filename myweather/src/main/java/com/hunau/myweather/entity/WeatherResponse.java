b8ae324d8d0b5e83b5a9cbcd2c0fe4fd4e950cfb
yangyu <973591208@qq.com>
2019-10-12 17:05:45 +0800

    天气预报

---
+++ myweather/src/main/java/com/hunau/myweather/entity/WeatherResponse.java
@@ -0,0 +1,28 @@
+package com.hunau.myweather.entity;
+
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+
+@Data
+public class WeatherResponse implements Serializable {
+
+
+
+    /**
+     * 消息数据
+     */
+    private Weather data;
+
+    /**
+     * 消息状态
+     */
+    private String status;
+
+    /**
+     * 消息描述
+     */
+    private String desc;
+}
